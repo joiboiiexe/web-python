4111431ada3b60bc135f3cb1eb2b99e82bc1f1a1
joiboiiexe <joiboiexe@gmail.com>
2025-01-03 19:51:33 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = "your_secret_key"
+    CLOUDINARY_CLOUD_NAME = "dzv3xgdso"
+    CLOUDINARY_API_KEY = "995211288265947"
+    CLOUDINARY_API_SECRET = "0bJX7uz7v58psFHaP3qT6pukUyQ"
